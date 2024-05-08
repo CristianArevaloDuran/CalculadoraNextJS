2fe5cca79cb592b62ee1ea35606be1db86ffc17f
Cristian Arevalo <cristianarevaloduran@gmail.com>
2024-05-08 13:40:02 -0500

    Workiing calculator

--- app/components/Calculadora/Calculadora.jsx
+++ app/components/Calculadora/Calculadora.jsx
@@ -1,44 +1,64 @@
 'use client'
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './calculadora.css';
-
-
 
 
 export default function Calculadora() {
     
+    const [disVal, setDisVal] = useState('');
     const display = useRef(null);
+    
+    const handleClick = (value) => {
+        if (value === '=') {
+            setDisVal(eval(disVal).toString())
+            display.current.scrollLeft = display.current.scrollWidth;
+        } else {
+            setDisVal(disVal+value)
+            display.current.scrollLeft = display.current.scrollWidth;
+        }
+    }
+    
+    const clearDisplay = () => {
+        setDisVal('');
+        display.current.scrollLeft = display.current.scrollWidth;
+    }
+
+    const delDisplay = () => {
+        setDisVal(disVal.slice(0, -1));
+        display.current.scrollLeft = display.current.scrollWidth;
+    }
+
 
     return (
-        <div class="calculadora">
-            <div class="display">
-                <span class="after" id="copy">
-                    <input value='' type="text" ref={display} disabled />
+        <div className="calculadora">
+            <div className="display">
+                <span className="after" id="copy">
+                    <input value={disVal} type="text" ref={display} disabled />
                 </span>
             </div>
-            <div class="keys">
-                <button class="key" onclick=''>7</button>
-                <button class="key" onclick=''>8</button>
-                <button class="key" onclick=''>9</button>
-                <button class="key" onclick=''>/</button>
-                <button class="key" onclick=''>4</button>
-                <button class="key" onclick=''>5</button>
-                <button class="key" onclick=''>6</button>
-                <button class="key" onclick=''>x</button>
-                <button class="key" onclick=''>1</button>
-                <button class="key" onclick=''>2</button>
-                <button class="key" onclick=''>3</button>
-                <button class="key" onclick=''>-</button>
-                <button class="key" onclick=''>0</button>
-                <button class="key" onclick=''>.</button>
-                <button class="key" onclick=''>=</button>
-                <button class="key" onclick=''>+</button>
-                <button class="key" onclick=''>
+            <div className="keys">
+                <button className="key" onClick={()=> handleClick('7')}>7</button>
+                <button className="key" onClick={()=> handleClick('8')}>8</button>
+                <button className="key" onClick={()=> handleClick('9')}>9</button>
+                <button className="key" onClick={()=> handleClick('/')}>/</button>
+                <button className="key" onClick={()=> handleClick('4')}>4</button>
+                <button className="key" onClick={()=> handleClick('5')}>5</button>
+                <button className="key" onClick={()=> handleClick('6')}>6</button>
+                <button className="key" onClick={()=> handleClick('*')}>x</button>
+                <button className="key" onClick={()=> handleClick('1')}>1</button>
+                <button className="key" onClick={()=> handleClick('2')}>2</button>
+                <button className="key" onClick={()=> handleClick('3')}>3</button>
+                <button className="key" onClick={()=> handleClick('-')}>-</button>
+                <button className="key" onClick={()=> handleClick('0')}>0</button>
+                <button className="key" onClick={()=> handleClick('.')}>.</button>
+                <button className="key" onClick={()=> handleClick('=')}>=</button>
+                <button className="key" onClick={()=> handleClick('+')}>+</button>
+                <button className="key" onClick={()=> clearDisplay()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                     C
                 </button>
-                <button class="key" onclick=''>
+                <button className="key" onClick={()=> delDisplay()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-200q-20 0-37.5-9T294-234L120-480l174-246q11-16 28.5-25t37.5-9h400q33 0 56.5 23.5T840-680v400q0 33-23.5 56.5T760-200H360Zm400-80v-400 400Zm-400 0h400v-400H360L218-480l142 200Zm96-40 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Z"/></svg>
                     Del
                 </button>
--- app/estadistica/page.jsx
+++ app/estadistica/page.jsx
@@ -7,21 +7,26 @@ import { useRef } from 'react';
 export default function Estadistica() {
 
     const calc = useRef(null);
+    const btn = useRef(null);
 
     const show = () => {
         calc.current.classList.toggle('active');
+        btn.current.classList.toggle('active');
     }
 
     return (
         <div className="container">
-            <h1>Estadistica</h1>
             <div className="floatCalc">
                 <div className="hide" ref={calc}>
                     <Calculadora />
                 </div>
                 <div className="calcBtn" onClick={show}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M320-240h60v-80h80v-60h-80v-80h-60v80h-80v60h80v80Zm200-30h200v-60H520v60Zm0-100h200v-60H520v60Zm44-152 56-56 56 56 42-42-56-58 56-56-42-42-56 56-56-56-42 42 56 56-56 58 42 42Zm-314-70h200v-60H250v60Zm-50 472q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>
+                    <svg ref={btn} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M320-240h60v-80h80v-60h-80v-80h-60v80h-80v60h80v80Zm200-30h200v-60H520v60Zm0-100h200v-60H520v60Zm44-152 56-56 56 56 42-42-56-58 56-56-42-42-56 56-56-56-42 42 56 56-56 58 42 42Zm-314-70h200v-60H250v60Zm-50 472q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>
                 </div>
+            </div>
+            <div className='inputContainer'>
+                <p>Ingrese los datos separados por comas</p>
+                <textarea name="" id="" cols="30" rows="10" placeholder='Ej. 20, 30, 21 ...'></textarea>
             </div>
         </div>
     );
