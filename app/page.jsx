be7601083cb3549860c74c7166d7a6bcc531fc56
Cristian Arevalo <cristianarevaloduran@gmail.com>
2024-05-22 23:42:16 -0500

    1.0 🥳

--- app/page.jsx
+++ app/page.jsx
@@ -6,26 +6,30 @@ export default function Page() {
   return (
     <div className='landContainer'>
         <h1 className='landTitle'>
-          Calculadora, calculos estadisticos y fisicos
+          Calculadora, calculos estadísticos y físicos
         </h1>
         <div className='landContent'>
-          <p className='explain'>
-            Esta es una calculadora que te ayudara a realizar calculos estadisticos y fisicos.
-          </p>
-          <p className='explain'>
-            Para comenzar, selecciona una de las opciones del menu.
-          </p>
-          <div className='serviceCards'>
-            <Link href='/calculadora' className='card'>
-              <p>Calculadora</p>
-              <img src="./imgs/calc.png" alt="calculadora" draggable='false' />
-            </Link>
-            <Link href='/estadistica' className='card'>
-              <p>Estadística</p>
-            </Link>
-            <Link href='/fisica' className='card'>
-              <p>Física</p>
-            </Link>
+          <div className='content'>  
+            <p className='explain'>
+              Esta es una calculadora que te ayudara a realizar calculos estadísticos y conversión de magnitudes físicas.
+            </p>
+            <p className='explain'>
+              Para comenzar, selecciona una de las opciones del menu, o usa la barra de navegación en la esquina superior izquierda.
+            </p>
+            <div className='serviceCards'>
+              <Link href='/calculadora' className='card'>
+                <p>Calculadora</p>
+                <img src="./imgs/calc.png" alt="calculadora" draggable='false' />
+              </Link>
+              <Link href='/estadistica' className='card'>
+                <p>Estadística</p>
+                <img src="./imgs/estats.png" alt="calculadora" draggable='false' />
+              </Link>
+              <Link href='/fisica' className='card'>
+                <p>Física</p>
+                <img src="./imgs/physics.png" alt="calculadora" draggable='false' />
+              </Link>
+          </div>
           </div>
         </div>
     </div>
